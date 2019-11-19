3ff9e2acbb2cc617e535309c2bada42947821720
Matt Beachey <mattbeachey@gmail.com>
2019-11-19 00:20:08 -0600

    quiz now generates questions and answers from the array, logs correct and incorrect responces

--- game-data.js
+++ game-data.js
@@ -1,83 +1,83 @@
-let questions1 = [
-    { Answer:"right answer", wrongAnswer: false },
-    { Answer:"wrong answer", wrongAnswer: true },
-    { Answer:"wrong answer", wrongAnswer: true },
-    { Answer:"wrong answer", wrongAnswer: true }
+let question1 = [
+    { Answer:"right answer", rightAnswer: true },
+    { Answer:"wrong answer", rightAnswer: false },
+    { Answer:"wrong answer", rightAnswer: false },
+    { Answer:"wrong answer", rightAnswer: false }
 ];
 
-for (let i = questions1.length - 1; i > 0; i--) {
+for (let i = question1.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * i)
-    const temp = questions1[i]
-    questions1[i] = questions1[j]
-    questions1[j] = temp
+    const temp = question1[i]
+    question1[i] = question1[j]
+    question1[j] = temp
 }
 
-console.log(questions1)
+//console.log(question1)
 
 
-let questions2 = [
-    { Answer:"right answer", wrongAnswer: false },
-    { Answer:"wrong answer", wrongAnswer: true },
-    { Answer:"wrong answer", wrongAnswer: true },
-    { Answer:"wrong answer", wrongAnswer: true }
+let question2 = [
+    { Answer:"right answer", rightAnswer: true },
+    { Answer:"wrong answer", rightAnswer: false },
+    { Answer:"wrong answer", rightAnswer: false },
+    { Answer:"wrong answer", rightAnswer: false }
 ];
 
-for (let i = questions2.length - 1; i > 0; i--) {
+for (let i = question2.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * i)
-    const temp = questions2[i]
-    questions2[i] = questions2[j]
-    questions2[j] = temp
+    const temp = question2[i]
+    question2[i] = question2[j]
+    question2[j] = temp
 }
 
-console.log(questions2)
+//console.log(question2)
 
 
-let questions3 = [
-    { Answer:"right answer", wrongAnswer: false },
-    { Answer:"wrong answer", wrongAnswer: true },
-    { Answer:"wrong answer", wrongAnswer: true },
-    { Answer:"wrong answer", wrongAnswer: true }
+let question3 = [
+    { Answer:"right answer", rightAnswer: true },
+    { Answer:"wrong answer", rightAnswer: false },
+    { Answer:"wrong answer", rightAnswer: false },
+    { Answer:"wrong answer", rightAnswer: false }
 ];
 
-for (let i = questions3.length - 1; i > 0; i--) {
+for (let i = question3.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * i)
-    const temp = questions3[i]
-    questions3[i] = questions3[j]
-    questions3[j] = temp
+    const temp = question3[i]
+    question3[i] = question3[j]
+    question3[j] = temp
 }
 
-console.log(questions3)
+//console.log(question3)
 
 
-let questions4 = [
-    { Answer:"right answer", wrongAnswer: false },
-    { Answer:"wrong answer", wrongAnswer: true },
-    { Answer:"wrong answer", wrongAnswer: true },
-    { Answer:"wrong answer", wrongAnswer: true }
+let question4 = [
+    { Answer:"right answer", rightAnswer: true },
+    { Answer:"wrong answer", rightAnswer: false },
+    { Answer:"wrong answer", rightAnswer: false },
+    { Answer:"wrong answer", rightAnswer: false }
 ];
 
-for (let i = questions4.length - 1; i > 0; i--) {
+for (let i = question4.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * i)
-    const temp = questions4[i]
-    questions4[i] = questions4[j]
-    questions4[j] = temp
+    const temp = question4[i]
+    question4[i] = question4[j]
+    question4[j] = temp
 }
 
-console.log(questions4)
+//console.log(question4)
 
 
-let questions5 = [
-    { Answer:"right answer", wrongAnswer: false },
-    { Answer:"wrong answer", wrongAnswer: true },
-    { Answer:"wrong answer", wrongAnswer: true },
-    { Answer:"wrong answer", wrongAnswer: true }
+let question5 = [
+    { Answer:"right answer", rightAnswer: false },
+    { Answer:"wrong answer", rightAnswer: false },
+    { Answer:"wrong answer", rightAnswer: false },
+    { Answer:"wrong answer", rightAnswer: false }
 ];
 
-for (let i = questions5.length - 1; i > 0; i--) {
+for (let i = question5.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * i)
-    const temp = questions5[i]
-    questions5[i] = questions5[j]
-    questions5[j] = temp
+    const temp = question5[i]
+    question5[i] = question5[j]
+    question5[j] = temp
 }
 
-console.log(questions5)
+//console.log(question5)
--- script.js
+++ script.js
@@ -15,36 +15,45 @@ function startGame() {
 
     document.getElementById("start-button").classList.add("disappear")
 
-    questions1.forEach(function () {
-        const buttonEl = document.createElement("button");
-        buttonEl.innerHTML = "click bait";
-        buttonEl.setAttribute("class", "btn btn-primary btn-lg");
-        document.body.append(buttonEl);
+    question1.forEach(function (question, i) {  //This foreach function is generating an answer based off of the array "Question1", creating a button for each array item
+        console.log(question)                   //in the above, are we creating "question?" It hasn't existed before, in now means a single part of the array
+        const answerEl = document.createElement("button");
+        answerEl.innerHTML = question.Answer; // this asigns each question to each button
+        answerEl.setAttribute("class", "button");
+        document.body.append(answerEl); //determine button's final destination on the page here
+        answerEl.addEventListener("click", function(){
+            if (question.rightAnswer === true) {
+                console.log("Correct Answer")
+            }
+            if (question.rightAnswer === false) {
+                console.log("Wrong Answer")
+            }
+        })
     });
 
-let i = 0;
-function myLoop() {
-    setTimeout(function () {
-        timer--
-        i++;
-        if (wrongAnsweri === true) {
-            i = i + 15
-            wrongAnsweri = false;
+    let i = 0;
+    function myLoop() {
+        setTimeout(function () {
+            timer--
+            i++;
+            if (wrongAnsweri === true) {
+                i = i + 15
+                wrongAnsweri = false;
+            }
+            if (i < 101) {
+                myLoop();
+                // console.log(i);
+            }
+        }, 1000)
+        if (wrongAnswer === true) {
+            timer = timer - 15
+            wrongAnswer = false;
         }
-        if (i < 101) {
-            myLoop();
-            console.log(i);
-        }
-    }, 1000)
-    if (wrongAnswer === true) {
-        timer = timer - 15
-        wrongAnswer = false;
+        // console.log(timer);
+        let timerString = timer.toString();
+        document.getElementById("timer").innerText = timerString
     }
-    console.log(timer);
-    let timerString = timer.toString();
-    document.getElementById("timer").innerText = timerString
-}
-myLoop();
+    myLoop();
 }
 
 
